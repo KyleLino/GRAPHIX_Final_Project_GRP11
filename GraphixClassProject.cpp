4ff6e79a78728313d24faa441821ae1fa2a39e36
Domsanity <95726513+Domsanity@users.noreply.github.com>
2022-12-07 20:36:33 +0800

    v0.5
    Added objects 2 - 7

--- GraphixClassProject.cpp
+++ GraphixClassProject.cpp
@@ -22,6 +22,11 @@ Camera cameraObject;
 Light lightObject;
 Model sampleObject;
 Model sampleObject2;
+Model sampleObject3;
+Model sampleObject4;
+Model sampleObject5;
+Model sampleObject6;
+Model sampleObject7;
 
 float mod_x = 0.0f;
 float mod_y = 0.0f;
@@ -291,6 +296,7 @@ int main(void)
         fullVertexData.push_back(attributes.texcoords[uvIndex]);
         fullVertexData.push_back(attributes.texcoords[uvIndex+1]);
     }
+
     std::string path2 = "3D/Octopus.obj";
     std::vector<tinyobj::shape_t> shapes2;
     std::vector<tinyobj::material_t> materials2;
@@ -328,6 +334,197 @@ int main(void)
         fullVertexData2.push_back(attributes.texcoords[uvIndex2]);
         fullVertexData2.push_back(attributes.texcoords[uvIndex2 + 1]);
     }
+
+    std::string path3 = "3D/bass.obj";
+    std::vector<tinyobj::shape_t> shapes3;
+    std::vector<tinyobj::material_t> materials3;
+    std::string warning3, error3;
+    tinyobj::attrib_t attributes3;
+    bool success3 = tinyobj::LoadObj(
+        &attributes3,
+        &shapes3,
+        &materials3,
+        &warning3,
+        &error3,
+        path3.c_str()
+    );
+    std::vector<GLuint> mesh_indices3;
+    for (int i = 0; i < shapes3[0].mesh.indices.size(); i++) {
+        mesh_indices3.push_back(
+            shapes3[0].mesh.indices[i].vertex_index
+        );
+    }
+    std::vector<GLfloat> fullVertexData3;
+    for (int i = 0; i < shapes3[0].mesh.indices.size(); i++) {
+        tinyobj::index_t vData3 = shapes3[0].mesh.indices[i];
+        int vertexIndex3 = vData3.vertex_index * 3;
+        int normalsIndex3 = vData3.normal_index * 3;
+        int uvIndex3 = vData3.texcoord_index * 2;
+
+        fullVertexData3.push_back(attributes3.vertices[vertexIndex3]);
+        fullVertexData3.push_back(attributes3.vertices[vertexIndex3 + 1]);
+        fullVertexData3.push_back(attributes3.vertices[vertexIndex3 + 2]);
+
+        fullVertexData3.push_back(attributes3.normals[normalsIndex3]);
+        fullVertexData3.push_back(attributes3.normals[normalsIndex3 + 1]);
+        fullVertexData3.push_back(attributes3.normals[normalsIndex3 + 2]);
+
+        fullVertexData3.push_back(attributes3.texcoords[uvIndex3]);
+        fullVertexData3.push_back(attributes3.texcoords[uvIndex3 + 1]);
+    }
+
+    std::string path4 = "3D/boat.obj";
+    std::vector<tinyobj::shape_t> shapes4;
+    std::vector<tinyobj::material_t> materials4;
+    std::string warning4, error4;
+    tinyobj::attrib_t attributes4;
+    bool success4 = tinyobj::LoadObj(
+        &attributes4,
+        &shapes4,
+        &materials4,
+        &warning4,
+        &error4,
+        path4.c_str()
+    );
+    std::vector<GLuint> mesh_indices4;
+    for (int i = 0; i < shapes4[0].mesh.indices.size(); i++) {
+        mesh_indices4.push_back(
+            shapes4[0].mesh.indices[i].vertex_index
+        );
+    }
+    std::vector<GLfloat> fullVertexData4;
+    for (int i = 0; i < shapes4[0].mesh.indices.size(); i++) {
+        tinyobj::index_t vData4 = shapes4[0].mesh.indices[i];
+        int vertexIndex4 = vData4.vertex_index * 3;
+        int normalsIndex4 = vData4.normal_index * 3;
+        int uvIndex4 = vData4.texcoord_index * 2;
+
+        fullVertexData4.push_back(attributes4.vertices[vertexIndex4]);
+        fullVertexData4.push_back(attributes4.vertices[vertexIndex4 + 1]);
+        fullVertexData4.push_back(attributes4.vertices[vertexIndex4 + 2]);
+
+        fullVertexData4.push_back(attributes4.normals[normalsIndex4]);
+        fullVertexData4.push_back(attributes4.normals[normalsIndex4 + 1]);
+        fullVertexData4.push_back(attributes4.normals[normalsIndex4 + 2]);
+
+        fullVertexData4.push_back(attributes4.texcoords[uvIndex4]);
+        fullVertexData4.push_back(attributes4.texcoords[uvIndex4 + 1]);
+    }
+
+    std::string path5 = "3D/bamboo_shark.obj";
+    std::vector<tinyobj::shape_t> shapes5;
+    std::vector<tinyobj::material_t> materials5;
+    std::string warning5, error5;
+    tinyobj::attrib_t attributes5;
+    bool success5 = tinyobj::LoadObj(
+        &attributes5,
+        &shapes5,
+        &materials5,
+        &warning5,
+        &error5,
+        path5.c_str()
+    );
+    std::vector<GLuint> mesh_indices5;
+    for (int i = 0; i < shapes5[0].mesh.indices.size(); i++) {
+        mesh_indices5.push_back(
+            shapes5[0].mesh.indices[i].vertex_index
+        );
+    }
+    std::vector<GLfloat> fullVertexData5;
+    for (int i = 0; i < shapes5[0].mesh.indices.size(); i++) {
+        tinyobj::index_t vData5 = shapes5[0].mesh.indices[i];
+        int vertexIndex5 = vData5.vertex_index * 3;
+        int normalsIndex5 = vData5.normal_index * 3;
+        int uvIndex5 = vData5.texcoord_index * 2;
+
+        fullVertexData5.push_back(attributes5.vertices[vertexIndex5]);
+        fullVertexData5.push_back(attributes5.vertices[vertexIndex5 + 1]);
+        fullVertexData5.push_back(attributes5.vertices[vertexIndex5 + 2]);
+
+        fullVertexData5.push_back(attributes5.normals[normalsIndex5]);
+        fullVertexData5.push_back(attributes5.normals[normalsIndex5 + 1]);
+        fullVertexData5.push_back(attributes5.normals[normalsIndex5 + 2]);
+
+        fullVertexData5.push_back(attributes5.texcoords[uvIndex5]);
+        fullVertexData5.push_back(attributes5.texcoords[uvIndex5 + 1]);
+    }
+
+    std::string path6 = "3D/person.obj";
+    std::vector<tinyobj::shape_t> shapes6;
+    std::vector<tinyobj::material_t> materials6;
+    std::string warning6, error6;
+    tinyobj::attrib_t attributes6;
+    bool success6 = tinyobj::LoadObj(
+        &attributes6,
+        &shapes6,
+        &materials6,
+        &warning6,
+        &error6,
+        path6.c_str()
+    );
+    std::vector<GLuint> mesh_indices6;
+    for (int i = 0; i < shapes6[0].mesh.indices.size(); i++) {
+        mesh_indices6.push_back(
+            shapes6[0].mesh.indices[i].vertex_index
+        );
+    }
+    std::vector<GLfloat> fullVertexData6;
+    for (int i = 0; i < shapes6[0].mesh.indices.size(); i++) {
+        tinyobj::index_t vData6 = shapes6[0].mesh.indices[i];
+        int vertexIndex6 = vData6.vertex_index * 3;
+        int normalsIndex6 = vData6.normal_index * 3;
+        int uvIndex6 = vData6.texcoord_index * 2;
+
+        fullVertexData6.push_back(attributes6.vertices[vertexIndex6]);
+        fullVertexData6.push_back(attributes6.vertices[vertexIndex6 + 1]);
+        fullVertexData6.push_back(attributes6.vertices[vertexIndex6 + 2]);
+
+        fullVertexData6.push_back(attributes6.normals[normalsIndex6]);
+        fullVertexData6.push_back(attributes6.normals[normalsIndex6 + 1]);
+        fullVertexData6.push_back(attributes6.normals[normalsIndex6 + 2]);
+
+        fullVertexData6.push_back(attributes6.texcoords[uvIndex6]);
+        fullVertexData6.push_back(attributes6.texcoords[uvIndex6 + 1]);
+    }
+
+    std::string path7 = "3D/Shark.obj";
+    std::vector<tinyobj::shape_t> shapes7;
+    std::vector<tinyobj::material_t> materials7;
+    std::string warning7, error7;
+    tinyobj::attrib_t attributes7;
+    bool success7 = tinyobj::LoadObj(
+        &attributes7,
+        &shapes7,
+        &materials7,
+        &warning7,
+        &error7,
+        path7.c_str()
+    );
+    std::vector<GLuint> mesh_indices7;
+    for (int i = 0; i < shapes7[0].mesh.indices.size(); i++) {
+        mesh_indices7.push_back(
+            shapes7[0].mesh.indices[i].vertex_index
+        );
+    }
+    std::vector<GLfloat> fullVertexData7;
+    for (int i = 0; i < shapes7[0].mesh.indices.size(); i++) {
+        tinyobj::index_t vData7 = shapes7[0].mesh.indices[i];
+        int vertexIndex7 = vData7.vertex_index * 3;
+        int normalsIndex7 = vData7.normal_index * 3;
+        int uvIndex7 = vData7.texcoord_index * 2;
+
+        fullVertexData7.push_back(attributes7.vertices[vertexIndex7]);
+        fullVertexData7.push_back(attributes7.vertices[vertexIndex7 + 1]);
+        fullVertexData7.push_back(attributes7.vertices[vertexIndex7 + 2]);
+
+        fullVertexData7.push_back(attributes7.normals[normalsIndex7]);
+        fullVertexData7.push_back(attributes7.normals[normalsIndex7 + 1]);
+        fullVertexData7.push_back(attributes7.normals[normalsIndex7 + 2]);
+
+        fullVertexData7.push_back(attributes7.texcoords[uvIndex7]);
+        fullVertexData7.push_back(attributes7.texcoords[uvIndex7 + 1]);
+    }
+
     GLfloat vertices[]{
         0.0f, 0.5f, 0.0f,
         -0.5f, -0.5f, 0.0f,
@@ -422,6 +619,221 @@ int main(void)
     glBindBuffer(GL_ARRAY_BUFFER, 0);
     glBindVertexArray(0);
 
+    GLuint VAO3, VBO3;
+    glGenVertexArrays(1, &VAO3);
+    glGenBuffers(1, &VBO3);
+    glBindVertexArray(VAO3);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO3);
+    glBufferData(
+        GL_ARRAY_BUFFER,
+        sizeof(GL_FLOAT)* fullVertexData3.size(),
+        fullVertexData3.data(),
+        GL_STATIC_DRAW
+    );
+    glVertexAttribPointer(
+        0,
+        3,
+        GL_FLOAT,
+        GL_FALSE,
+        8 * sizeof(GL_FLOAT),
+        (void*)0
+    );
+    GLintptr normPtr3 = 3 * sizeof(GL_FLOAT);
+    glVertexAttribPointer(
+        1,
+        3,
+        GL_FLOAT,
+        GL_FALSE,
+        8 * sizeof(GL_FLOAT),
+        (void*)normPtr3
+    );
+    GLintptr uvPtr3 = 6 * sizeof(GL_FLOAT);
+    glVertexAttribPointer(
+        2,
+        2,
+        GL_FLOAT,
+        GL_FALSE,
+        8 * sizeof(GL_FLOAT),
+        (void*)uvPtr3
+    );
+    glEnableVertexAttribArray(0);
+    glEnableVertexAttribArray(1);
+    glEnableVertexAttribArray(2);
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
+
+    GLuint VAO4, VBO4;
+    glGenVertexArrays(1, &VAO4);
+    glGenBuffers(1, &VBO4);
+    glBindVertexArray(VAO4);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO4);
+    glBufferData(
+        GL_ARRAY_BUFFER,
+        sizeof(GL_FLOAT)* fullVertexData4.size(),
+        fullVertexData4.data(),
+        GL_STATIC_DRAW
+    );
+    glVertexAttribPointer(
+        0,
+        3,
+        GL_FLOAT,
+        GL_FALSE,
+        8 * sizeof(GL_FLOAT),
+        (void*)0
+    );
+    GLintptr normPtr4 = 3 * sizeof(GL_FLOAT);
+    glVertexAttribPointer(
+        1,
+        3,
+        GL_FLOAT,
+        GL_FALSE,
+        8 * sizeof(GL_FLOAT),
+        (void*)normPtr4
+    );
+    GLintptr uvPtr4 = 6 * sizeof(GL_FLOAT);
+    glVertexAttribPointer(
+        2,
+        2,
+        GL_FLOAT,
+        GL_FALSE,
+        8 * sizeof(GL_FLOAT),
+        (void*)uvPtr4
+    );
+    glEnableVertexAttribArray(0);
+    glEnableVertexAttribArray(1);
+    glEnableVertexAttribArray(2);
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
+
+    GLuint VAO5, VBO5;
+    glGenVertexArrays(1, &VAO5);
+    glGenBuffers(1, &VBO5);
+    glBindVertexArray(VAO5);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO5);
+    glBufferData(
+        GL_ARRAY_BUFFER,
+        sizeof(GL_FLOAT)* fullVertexData5.size(),
+        fullVertexData5.data(),
+        GL_STATIC_DRAW
+    );
+    glVertexAttribPointer(
+        0,
+        3,
+        GL_FLOAT,
+        GL_FALSE,
+        8 * sizeof(GL_FLOAT),
+        (void*)0
+    );
+    GLintptr normPtr5 = 3 * sizeof(GL_FLOAT);
+    glVertexAttribPointer(
+        1,
+        3,
+        GL_FLOAT,
+        GL_FALSE,
+        8 * sizeof(GL_FLOAT),
+        (void*)normPtr5
+    );
+    GLintptr uvPtr5 = 6 * sizeof(GL_FLOAT);
+    glVertexAttribPointer(
+        2,
+        2,
+        GL_FLOAT,
+        GL_FALSE,
+        8 * sizeof(GL_FLOAT),
+        (void*)uvPtr5
+    );
+    glEnableVertexAttribArray(0);
+    glEnableVertexAttribArray(1);
+    glEnableVertexAttribArray(2);
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
+
+    GLuint VAO6, VBO6;
+    glGenVertexArrays(1, &VAO6);
+    glGenBuffers(1, &VBO6);
+    glBindVertexArray(VAO6);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO6);
+    glBufferData(
+        GL_ARRAY_BUFFER,
+        sizeof(GL_FLOAT)* fullVertexData6.size(),
+        fullVertexData6.data(),
+        GL_STATIC_DRAW
+    );
+    glVertexAttribPointer(
+        0,
+        3,
+        GL_FLOAT,
+        GL_FALSE,
+        8 * sizeof(GL_FLOAT),
+        (void*)0
+    );
+    GLintptr normPtr6 = 3 * sizeof(GL_FLOAT);
+    glVertexAttribPointer(
+        1,
+        3,
+        GL_FLOAT,
+        GL_FALSE,
+        8 * sizeof(GL_FLOAT),
+        (void*)normPtr6
+    );
+    GLintptr uvPtr6 = 6 * sizeof(GL_FLOAT);
+    glVertexAttribPointer(
+        2,
+        2,
+        GL_FLOAT,
+        GL_FALSE,
+        8 * sizeof(GL_FLOAT),
+        (void*)uvPtr6
+    );
+    glEnableVertexAttribArray(0);
+    glEnableVertexAttribArray(1);
+    glEnableVertexAttribArray(2);
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
+
+    GLuint VAO7, VBO7;
+    glGenVertexArrays(1, &VAO7);
+    glGenBuffers(1, &VBO7);
+    glBindVertexArray(VAO7);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO7);
+    glBufferData(
+        GL_ARRAY_BUFFER,
+        sizeof(GL_FLOAT)* fullVertexData7.size(),
+        fullVertexData7.data(),
+        GL_STATIC_DRAW
+    );
+    glVertexAttribPointer(
+        0,
+        3,
+        GL_FLOAT,
+        GL_FALSE,
+        8 * sizeof(GL_FLOAT),
+        (void*)0
+    );
+    GLintptr normPtr7 = 3 * sizeof(GL_FLOAT);
+    glVertexAttribPointer(
+        1,
+        3,
+        GL_FLOAT,
+        GL_FALSE,
+        8 * sizeof(GL_FLOAT),
+        (void*)normPtr7
+    );
+    GLintptr uvPtr7 = 6 * sizeof(GL_FLOAT);
+    glVertexAttribPointer(
+        2,
+        2,
+        GL_FLOAT,
+        GL_FALSE,
+        8 * sizeof(GL_FLOAT),
+        (void*)uvPtr7
+    );
+    glEnableVertexAttribArray(0);
+    glEnableVertexAttribArray(1);
+    glEnableVertexAttribArray(2);
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
+
     glm::mat3 identity_matrix3 = glm::mat3(1.0f);
     glm::mat4 identity_matrix4 = glm::mat4(1.0f);
     float x, y, z;
@@ -472,8 +884,34 @@ int main(void)
         
         sampleObject2.setPosition(3.0f, 0.0f, 0.0f);
         sampleObject2.setRotation(0.0f, 1.0f, 0.0f);
-        sampleObject2.setScale(2.0f, 2.0f, 2.0f);
-        sampleObject2.drawObject(VAO2, skyboxVAO, texture2, skyboxTex, shaderProgram, skybox_shaderProgram, fullVertexData2.size(), cameraObject.getViewMatrix(), cameraObject.getPerspective(), cameraObject.getCameraPosition(), lightObject.getLightPosition(), lightObject.getLightColor(), lightObject.getAmbientStrength(), lightObject.getAmbientColor(), lightObject.getSpecStrength(), lightObject.getSpecPhong(), theta);
+        sampleObject2.setScale(1.0f, 1.0f, 1.0f);
+        sampleObject2.drawObject(VAO2, skyboxVAO, texture, skyboxTex, shaderProgram, skybox_shaderProgram, fullVertexData2.size(), cameraObject.getViewMatrix(), cameraObject.getPerspective(), cameraObject.getCameraPosition(), lightObject.getLightPosition(), lightObject.getLightColor(), lightObject.getAmbientStrength(), lightObject.getAmbientColor(), lightObject.getSpecStrength(), lightObject.getSpecPhong(), theta);
+        
+        sampleObject3.setPosition(-3.0f, 0.0f, 0.0f);
+        sampleObject3.setRotation(0.0f, 1.0f, 0.0f);
+        sampleObject3.setScale(0.5f, 0.5f, 0.5f);
+        sampleObject3.drawObject(VAO3, skyboxVAO, texture, skyboxTex, shaderProgram, skybox_shaderProgram, fullVertexData3.size(), cameraObject.getViewMatrix(), cameraObject.getPerspective(), cameraObject.getCameraPosition(), lightObject.getLightPosition(), lightObject.getLightColor(), lightObject.getAmbientStrength(), lightObject.getAmbientColor(), lightObject.getSpecStrength(), lightObject.getSpecPhong(), theta);
+
+        sampleObject4.setPosition(3.0f, 3.0f, 0.0f);
+        sampleObject4.setRotation(0.0f, 1.0f, 0.0f);
+        sampleObject4.setScale(1.0f, 1.0f, 1.0f);
+        sampleObject4.drawObject(VAO4, skyboxVAO, texture, skyboxTex, shaderProgram, skybox_shaderProgram, fullVertexData4.size(), cameraObject.getViewMatrix(), cameraObject.getPerspective(), cameraObject.getCameraPosition(), lightObject.getLightPosition(), lightObject.getLightColor(), lightObject.getAmbientStrength(), lightObject.getAmbientColor(), lightObject.getSpecStrength(), lightObject.getSpecPhong(), theta);
+
+        sampleObject5.setPosition(-3.0f, 3.0f, 0.0f);
+        sampleObject5.setRotation(0.0f, 1.0f, 0.0f);
+        sampleObject5.setScale(0.01f, 0.01f, 0.01f);
+        sampleObject5.drawObject(VAO5, skyboxVAO, texture, skyboxTex, shaderProgram, skybox_shaderProgram, fullVertexData5.size(), cameraObject.getViewMatrix(), cameraObject.getPerspective(), cameraObject.getCameraPosition(), lightObject.getLightPosition(), lightObject.getLightColor(), lightObject.getAmbientStrength(), lightObject.getAmbientColor(), lightObject.getSpecStrength(), lightObject.getSpecPhong(), theta);
+
+        sampleObject6.setPosition(3.0f, -3.0f, 0.0f);
+        sampleObject6.setRotation(0.0f, 1.0f, 0.0f);
+        sampleObject6.setScale(1.01f, 1.01f, 1.01f);
+        sampleObject6.drawObject(VAO6, skyboxVAO, texture, skyboxTex, shaderProgram, skybox_shaderProgram, fullVertexData6.size(), cameraObject.getViewMatrix(), cameraObject.getPerspective(), cameraObject.getCameraPosition(), lightObject.getLightPosition(), lightObject.getLightColor(), lightObject.getAmbientStrength(), lightObject.getAmbientColor(), lightObject.getSpecStrength(), lightObject.getSpecPhong(), theta);
+
+        sampleObject6.setPosition(-3.0f, -3.0f, 0.0f);
+        sampleObject6.setRotation(0.0f, 1.0f, 0.0f);
+        sampleObject6.setScale(0.05f, 0.05f, 0.05f);
+        sampleObject6.drawObject(VAO7, skyboxVAO, texture, skyboxTex, shaderProgram, skybox_shaderProgram, fullVertexData7.size(), cameraObject.getViewMatrix(), cameraObject.getPerspective(), cameraObject.getCameraPosition(), lightObject.getLightPosition(), lightObject.getLightColor(), lightObject.getAmbientStrength(), lightObject.getAmbientColor(), lightObject.getSpecStrength(), lightObject.getSpecPhong(), theta);
+
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
 
